22a28eb9fdb328d39a0b478ae5226f8395902f17
Deepak Singh <deepak16032@iiitd.ac.in>
2026-05-31 20:41:56 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = "".join(filter(str.isalnum, s)).lower()
+        return cleaned == cleaned[::-1]
+        
